cd10744a97147672c5c90e9e36b1718ded7e0528
omgshalihin <shalihinsgp@gmail.com>
2023-01-04 16:29:40 +0100

    updated solution

--- randomuser-react-typescript/src/App.tsx
+++ randomuser-react-typescript/src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { RandomUser } from './components/RandomUser';
 
 function App() {
+  const [user, setUser] = useState({
+    name: ' ',
+    age: 0,
+    address: '',
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://randomuser.me/api/');
+      const data = await response.json();
+      const firstUser = data.results[0];
+
+      setUser({
+        name: firstUser.name.first,
+        age: firstUser.dob.age,
+        address: firstUser.location.street.name,
+      });
+    };
+
+    getData();
+  }, []);
+
+  const handleNameChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setUser({ ...user, name: value });
+  };
+
   return (
-    <div className="App">
-      <RandomUser />
+    <div className='container'>
+      <h1><u>User Details</u></h1>
+      <RandomUser name={user.name} age={user.age} address={user.address} />
+      <input type="text" value={user.name} onChange={handleNameChange} />
     </div>
   );
 }
--- randomuser-react-typescript/src/components/RandomUser.tsx
+++ randomuser-react-typescript/src/components/RandomUser.tsx
@@ -1,57 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import './RandomUser.css';
+import React from 'react';
 
-const RandomUser = () => {
-  type User = {
-    name: string,
-    age: number,
-    address: string
-  };
-
-  const [userData, setUserData] = useState<User>({
-    name: ' ',
-    age: 0,
-    address: '',
-  });
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch('https://randomuser.me/api/');
-      const data = await response.json();
-      const firstUser = data.results[0];
-
-      setUserData({
-        name: firstUser.name.first,
-        age: firstUser.dob.age,
-        address: firstUser.location.street.name,
-      });
-    };
-
-    getData();
-  }, []);
-
-  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    const { value } = e.target;
-    setUserData({ ...userData, name: value });
-  };
-
-  return (
-    <div>
-
-      <div className='user--details__container'>
-        <h2><u>User Details</u></h2>
-        <h3>My name is {userData.name}.</h3>
-        <h3>I am {userData.age} years old.</h3>
-        <h3>I'm living at {userData.address}.</h3>
-      </div>
-
-      <form action="" className='form__container'>
-        <h2>Not you? Change the name of {userData.name} here.</h2>
-        <input type="text" value={userData.name} onChange={handleChange}/>
-      </form>
-    </div>
-  );
+type UserData = {
+  name: string,
+  age: number,
+  address: string
 };
+
+const RandomUser = ({ name, age, address } : UserData) => (
+    <div>
+      <h1>Hi, my name is {name}.</h1>
+      <h1>I am {age} years old.</h1>
+      <h1>I live in {address}.</h1>
+    </div>
+);
 
 export { RandomUser };
