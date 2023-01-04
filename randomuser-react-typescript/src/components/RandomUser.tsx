8519dd6e6c553dd8f20f805bb2589c36fa6cae0c
omgshalihin <shalihinsgp@gmail.com>
2023-01-04 14:10:34 +0100

    name, age, address showing

--- randomuser-react-typescript/src/components/RandomUser.tsx
+++ randomuser-react-typescript/src/components/RandomUser.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const RandomUser = () => (
-    <div>RandomUser</div>
-);
+const RandomUser = () => {
+  const [userData, setUserData] = useState({
+    name: ' ',
+    age: 0,
+    address: '',
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://randomuser.me/api/');
+      const data = await response.json();
+      const firstUser = data.results[0];
+      setUserData({
+        name: firstUser.name.first,
+        age: firstUser.dob.age,
+        address: firstUser.location.street.name,
+      });
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <div className='apple'>
+        <h2>User Details</h2>
+        <h3>My name is {userData.name}.</h3>
+        <h3>I am {userData.age} years old.</h3>
+        <h3>I'm living at {userData.address}.</h3>
+      </div>
+    </div>
+  );
+};
 
 export { RandomUser };
