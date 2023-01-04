5aba1cfa946998efb1893432b6f657b46acb7541
omgshalihin <shalihinsgp@gmail.com>
2023-01-04 12:59:27 +0100

    included "import/prefer-default-export": "off",

--- randomuser-react-typescript/src/App.tsx
+++ randomuser-react-typescript/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { RandomUser } from './components/RandomUser';
 
 function App() {
   return (
     <div className="App">
-      <h1>hey</h1>
+      <h1>This is the app</h1>
     </div>
   );
 }
