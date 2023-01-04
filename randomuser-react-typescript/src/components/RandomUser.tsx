51233a33bb46308e47a3a86c9e8874c740c2195d
omgshalihin <shalihinsgp@gmail.com>
2023-01-04 15:08:41 +0100

    username interchangable

--- randomuser-react-typescript/src/components/RandomUser.tsx
+++ randomuser-react-typescript/src/components/RandomUser.tsx
@@ -13,6 +13,7 @@ const RandomUser = () => {
       const response = await fetch('https://randomuser.me/api/');
       const data = await response.json();
       const firstUser = data.results[0];
+
       setUserData({
         name: firstUser.name.first,
         age: firstUser.dob.age,
@@ -23,14 +24,26 @@ const RandomUser = () => {
     getData();
   }, []);
 
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setUserData({ ...userData, name: value });
+  };
+
   return (
     <div>
+
       <div className='user--details__container'>
         <h2><u>User Details</u></h2>
         <h3>My name is {userData.name}.</h3>
         <h3>I am {userData.age} years old.</h3>
         <h3>I'm living at {userData.address}.</h3>
       </div>
+
+      <form action="" className='form__container'>
+        <h2>Not you? Change the name of {userData.name} here.</h2>
+        <input type="text" value={userData.name} onChange={handleChange}/>
+      </form>
     </div>
   );
 };
