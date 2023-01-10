a1cf961f54b8a9477fc296cf3d943f026e37a834
omgshalihin <shalihinsgp@gmail.com>
2023-01-10 19:09:23 +0100

    fetching data and displaying data

--- puppies-fullstack/src/App.tsx
+++ puppies-fullstack/src/App.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+type DataType = {
+  map(arg0: (puppy: any) => void): React.ReactNode;
+  breed: string,
+  name: string,
+  dob: string
+};
+
+const App = () => {
+  const [data, setData] = useState<DataType>();
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/puppies');
+        const newData = await response.json();
+        setData(newData);
+      } catch (err : any) {
+        setError(err.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (data === undefined) return <h1>Loading...</h1>;
+
+  if (error) return <h1>Please look at this error: {error}</h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Puppies
-        </p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+      <h1>Puppies</h1>
+      {data.map(puppy => (
+          <div className='puppy-details'>
+            <h2>Name: {puppy.name}</h2>
+            <h2>Breed: {puppy.breed}</h2>
+            <h2>DOB: {puppy.dob}</h2>
+          </div>
+      ))}
       </header>
     </div>
   );
-}
+};
 
 export default App;
